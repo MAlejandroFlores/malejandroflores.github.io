b9ea0c5ddeaf6ee462a231412df03b9c64d60e79
MAlejandroFlores <alexfloresm@byui.edu>
2021-02-22 17:43:57 -0800

    Assignment W07

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,3 +1,30 @@
-const images = document.querySelectorAll("src-img")
+const images = document.querySelectorAll('img[data-src]');
 
-const imgObserver = new IntersectionObserver()
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+    img.src = src;
+    img.removeAttribute('data-src');
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px -5px 0px "
+};
+
+const imgObserver = new IntersectionObserver(function(entries, imgObserver) {
+    entries.forEach(entry => {
+        //console.log(entry.target);
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+        //entry.target.classList.toggle("opacity");
+    });
+}, imgOptions);
+
+images.forEach(image => { imgObserver.observe(image); });

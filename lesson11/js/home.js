f59e97b32895ab6c822a85daa8a36b2558e002f8
MAlejandroFlores <alexfloresm@byui.edu>
2021-03-18 13:24:55 -0700

    footer color

--- lesson11/js/home.js
+++ lesson11/js/home.js
@@ -24,7 +24,7 @@ fetch(requestURL)
                 let p = document.createElement('p');
                 let r = document.createElement('p');
                 let image = document.createElement('img');
-                let alt = towns[i].name + "'s Image";
+                let alt = towns[i].name;
 
 
                 h2.textContent = towns[i].name;

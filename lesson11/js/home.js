94010229a84c70343cb943006b2655092b69eac8
MAlejandroFlores <alexfloresm@byui.edu>
2021-03-22 21:19:56 -0700

    css home page

--- lesson11/js/home.js
+++ lesson11/js/home.js
@@ -18,11 +18,15 @@ fetch(requestURL)
         for (let i = 0; i < towns.length; i++) {
             if (towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs') {
                 let card = document.createElement('section');
+                let textDiv = document.createElement('div');
+                textDiv.setAttribute('class', "cards-text-area");
                 let h2 = document.createElement('h2');
                 let h3 = document.createElement('h3');
                 let y = document.createElement('p');
                 let p = document.createElement('p');
                 let r = document.createElement('p');
+                let imgDiv = document.createElement('div');
+                imgDiv.setAttribute("class", "cards-img-area")
                 let image = document.createElement('img');
                 let alt = towns[i].name;
 
@@ -30,17 +34,28 @@ fetch(requestURL)
                 h2.textContent = towns[i].name;
                 h3.textContent = towns[i].motto;
                 y.textContent = 'Year Founded: ' + towns[i].yearFounded;
-                p.textContent = 'Populatio : ' + towns[i].currentPopulation;
+                p.textContent = 'Population : ' + towns[i].currentPopulation;
                 r.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
                 image.setAttribute('src', "images/" + towns[i].photo);
                 image.setAttribute('alt', alt)
 
-                card.appendChild(h2);
-                card.appendChild(h3);
-                card.appendChild(y);
-                card.appendChild(p);
-                card.appendChild(r);
-                card.appendChild(image);
+                // card.appendChild(h2);
+                // card.appendChild(h3);
+                // card.appendChild(y);
+                // card.appendChild(p);
+                // card.appendChild(r);
+                // card.appendChild(image);
+
+                textDiv.appendChild(h2);
+                textDiv.appendChild(h3);
+                textDiv.appendChild(y);
+                textDiv.appendChild(p);
+                textDiv.appendChild(r);
+
+                imgDiv.appendChild(image);
+
+                card.appendChild(textDiv);
+                card.appendChild(imgDiv);
 
                 document.querySelector('div.cards').appendChild(card);
             }

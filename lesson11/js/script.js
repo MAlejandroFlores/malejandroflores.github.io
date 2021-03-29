a6a860adbb6cab10dfe06b5eb6d3fe1da17428de
MAlejandroFlores <alexfloresm@byui.edu>
2021-03-29 16:17:09 -0700

    js range

--- lesson11/js/script.js
+++ lesson11/js/script.js
@@ -51,3 +51,7 @@ function toggleMenu() {
         .getElementsByClassName("nav_menu")[0]
         .classList.toggle("responsive");
 }
+
+function displayRateValue(rate) {
+    document.getElementById("severityValue").innerHTML = rate;
+}

6438fb43772bae2e3b00c7a8845c4eac264e304f
MAlejandroFlores <alexfloresm@byui.edu>
2021-04-04 18:49:15 -0700

    directpry cards

--- FinalProject/js/directory.js
+++ FinalProject/js/directory.js
@@ -7,6 +7,37 @@ function getDirectoryCards() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            const businesses = data['businesses'];
+            for (const business of businesses) {
+                let card = document.createElement('section');
+                let textInfo = document.createElement('div');
+                textInfo.setAttribute("class", "card_text_area");
+                let name = document.createElement('h2');
+                let contact = document.createElement('p');
+                contact.setAttribute("class", "b_contact");
+                let description = document.createElement('p');
+                description.setAttribute('class', "b_description");
+                let logo = document.createElement('div');
+                logo.setAttribute("class", "card_logo_area");
+                let image = document.createElement('img');
+
+                name.textContent = business.name;
+                contact.textContent = business.contact_info;
+                description.textContent = business.description;
+                image.setAttribute('src', 'images/business/' + business.logo_file);
+
+                textInfo.appendChild(name);
+                textInfo.appendChild(contact);
+                textInfo.appendChild(description);
+
+                logo.appendChild(image);
+
+                card.appendChild(textInfo);
+                card.appendChild(logo);
+
+
+                document.querySelector('.card_container').appendChild(card);
+            }
         });
 
     // fetch(requestURL)

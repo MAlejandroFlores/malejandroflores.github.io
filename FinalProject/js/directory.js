e96a3823c073bcea3b99188e9732609a7e41be12
MAlejandroFlores <alexfloresm@byui.edu>
2021-04-04 17:17:39 -0700

    add Info

---
+++ FinalProject/js/directory.js
@@ -0,0 +1,56 @@
+/* ---- FETCH DIRECTORY CARDS ---- */
+function getDirectoryCards() {
+    /* ---- FECTH JSON DATA ---- */
+    const requestURL = 'https://malejandroflores.github.io/FinalProject/common/directory.json';
+
+    fetch(requestURL)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(jsonObject) {
+            //console.table(jsonObject); // temporary checking for valid response and data parsing
+            const businessInfo = jsonObject['business'];
+            businessInfo.forEach(element => {
+
+            }); {
+                if (towns[i].name == 'Fish Haven' || towns[i].name == 'Preston' || towns[i].name == 'Soda Springs') {
+                    let card = document.createElement('section');
+                    let textDiv = document.createElement('div');
+                    textDiv.setAttribute('class', "cards-text-area");
+                    let h2 = document.createElement('h2');
+                    let h3 = document.createElement('h3');
+                    let y = document.createElement('p');
+                    let p = document.createElement('p');
+                    let r = document.createElement('p');
+                    let imgDiv = document.createElement('div');
+                    imgDiv.setAttribute("class", "cards-img-area")
+                    let image = document.createElement('img');
+                    let alt = towns[i].name;
+
+                    h2.textContent = towns[i].name;
+                    h3.textContent = towns[i].motto;
+                    y.textContent = 'Year Founded: ' + towns[i].yearFounded;
+                    p.textContent = 'Population : ' + towns[i].currentPopulation;
+                    r.textContent = 'Annual Rain Fall: ' + towns[i].averageRainfall;
+                    image.setAttribute('src', "images/" + towns[i].photo);
+                    image.setAttribute('alt', alt)
+
+
+                    textDiv.appendChild(h2);
+                    textDiv.appendChild(h3);
+                    textDiv.appendChild(y);
+                    textDiv.appendChild(p);
+                    textDiv.appendChild(r);
+
+                    imgDiv.appendChild(image);
+
+                    card.appendChild(textDiv);
+                    card.appendChild(imgDiv);
+
+                    document.querySelector('div.cards').appendChild(card);
+                }
+            }
+        });
+}
+
+getDirectoryCards();

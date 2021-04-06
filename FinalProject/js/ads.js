fe731f8b6082444f1a88932038a011b8f95e68f3
MAlejandroFlores <alexfloresm@byui.edu>
2021-04-05 22:31:05 -0700

    ads

---
+++ FinalProject/js/ads.js
@@ -0,0 +1,9 @@
+function insertAds() {
+    const getUrl = 'https://malejandroflores.github.io/FinalProject/common/directory.json';
+
+    fetch(getUrl)
+        .then(response => response.json())
+        .then(data =>
+
+        )
+}

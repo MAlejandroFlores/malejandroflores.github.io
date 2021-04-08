fd902b835c411d32ab84f206210c4eaf80d5ab24
MAlejandroFlores <alexfloresm@byui.edu>
2021-04-07 23:16:56 -0700

    fix errors

--- FinalProject/js/ads.js
+++ FinalProject/js/ads.js
@@ -28,6 +28,7 @@ function insertAds() {
                         phone.textContent = business.phone;
                         address.textContent = business.address;
                         image.setAttribute('src', 'images/business/' + business.logo_file);
+                        image.setAttribute('alt', business.name);
 
                         textInfo.appendChild(name);
                         textInfo.appendChild(phone);

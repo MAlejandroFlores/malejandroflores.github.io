8e516804d78f7e0e7477dad3da94f74d89171dcf
MAlejandroFlores <alexfloresm@byui.edu>
2021-04-10 19:50:07 -0700

    Link pages Dir

--- FinalProject/js/directory.js
+++ FinalProject/js/directory.js
@@ -6,12 +6,13 @@ function getDirectoryCards() {
     fetch(requestURL)
         .then(response => response.json())
         .then(data => {
-            // console.log(data);
+            console.log(data);
             const businesses = data['businesses'];
             for (const business of businesses) {
                 let card = document.createElement('section');
                 let textInfo = document.createElement('div');
                 textInfo.setAttribute("class", "card_text_area");
+                let nameLink = document.createElement('a');
                 let name = document.createElement('h2');
                 let phone = document.createElement('p');
                 phone.setAttribute("class", "b_phone");
@@ -19,6 +20,7 @@ function getDirectoryCards() {
                 address.setAttribute('class', "b_description");
                 let logo = document.createElement('div');
                 logo.setAttribute("class", "card_logo_area");
+                let imageLink = document.createElement('a');
                 let image = document.createElement('img');
 
                 name.textContent = business.name;
@@ -26,12 +28,19 @@ function getDirectoryCards() {
                 address.textContent = business.address;
                 image.setAttribute('src', 'images/business/' + business.logo_file);
                 image.setAttribute('alt', business.name);
+                nameLink.setAttribute('href', business.web_link);
+                imageLink.setAttribute('href', business.web_link);
+                nameLink.setAttribute('target', "_blank");
+                imageLink.setAttribute('target', "_blank");
+                nameLink.appendChild(name);
+                imageLink.appendChild(image);
 
-                textInfo.appendChild(name);
+                // textInfo.appendChild(name);
+                textInfo.appendChild(nameLink);
                 textInfo.appendChild(phone);
                 textInfo.appendChild(address);
 
-                logo.appendChild(image);
+                logo.appendChild(imageLink);
 
                 card.appendChild(textInfo);
                 card.appendChild(logo);
@@ -45,21 +54,21 @@ function getDirectoryCards() {
 }
 
 function toList() {
-    let card_container =  document.querySelector('.card_container');
+    let card_container = document.querySelector('.card_container');
 
     // let card_container = let card = document.createElement('section');;
-        card_container.classList.remove("card_grid");
-        card_container.classList.add("card_list");
-        
+    card_container.classList.remove("card_grid");
+    card_container.classList.add("card_list");
+
 }
 
 function toGrid() {
 
-    let card_container =  document.querySelector('.card_container');
+    let card_container = document.querySelector('.card_container');
 
     // let card_container = let card = document.createElement('section');;
-        card_container.classList.add("card_grid");
-        card_container.classList.remove("card_list");
+    card_container.classList.add("card_grid");
+    card_container.classList.remove("card_list");
 }
 
 getDirectoryCards();
